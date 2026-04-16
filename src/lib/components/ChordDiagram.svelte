2490cd7e35837f92d739deaf1d18f853fc0e7108
jbaldacim <jbaldacim@gmail.com>
2026-04-16 17:51:57 -0300

    first commit

---
+++ src/lib/components/ChordDiagram.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { labels, matrix } from '$lib/data/deputados-adj-matrix.json';
+	import { chord } from 'd3-chord';
+	import { descending } from 'd3-array';
+	let { width, height } = $props();
+
+	const chordGenerator = chord().padAngle(0.05).sortSubgroups(descending);
+	const chords = chordGenerator(matrix);
+</script>
+
+<svg {width} {height} fill="firebrick"></svg>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+	import ChordDiagram from '$lib/components/ChordDiagram.svelte';
+	let minWidth = $state(400);
+	let windowWidth = $state(0);
+
+	let width = $derived(Math.max(minWidth, windowWidth));
+	const height = 800;
+</script>
+
+<div bind:clientWidth={windowWidth}><ChordDiagram {width} {height} /></div>
+
+<style>
+	div {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		font-family: Helvetica, sans-serif;
+	}
+</style>
